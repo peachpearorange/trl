dec5e12ddcc58bf4a1daa412722d50b7a50de884
I <>
2026-05-28 13:12:35 +0000

    island: stairs down go to z=-1 layer of island.txt
    Underground now loaded from island.txt LEVEL -1 (multi-z editor format)
    instead of bigundergroundplace.txt. Also fixes the docking failure caused
    by parse_save not understanding the new ZRANGE/LEVEL save format.

--- src/locations/island.rs
+++ src/locations/island.rs
@@ -4,8 +4,7 @@ use crate::{entities::*,
 
 pub const ID: LocationId = (12, 0, 0);
 
-const SURFACE_DATA: &str = include_str!("../../editor_saves/island.txt");
-const UNDERGROUND_DATA: &str = include_str!("../../editor_saves/bigundergroundplace.txt");
+const ISLAND_DATA: &str = include_str!("../../editor_saves/island.txt");
 
 fn object_from_save(name: &str) -> Option<Object> {
   Some(match name {
@@ -50,16 +49,20 @@ struct ParsedLevel {
   objects: Vec<(i32, i32, Object)>
 }
 
-fn parse_save(data: &str) -> ParsedLevel {
+fn parse_save(data: &str, target_z: i32) -> ParsedLevel {
   let (grid, _markers) = data.split_once("MARKERS\n").unwrap_or((data, ""));
-  let mut toks = grid.split_whitespace();
-  let w: usize = toks.next().and_then(|s| s.parse().ok()).unwrap_or(0);
-  let h: usize = toks.next().and_then(|s| s.parse().ok()).unwrap_or(0);
-  let _ox: i32 = toks.next().and_then(|s| s.parse().ok()).unwrap_or(0);
-  let _oy: i32 = toks.next().and_then(|s| s.parse().ok()).unwrap_or(0);
+  let mut lines = grid.lines();
+  let header = lines.next().unwrap_or("");
+  let mut hi = header.split_whitespace();
+  let w: usize = hi.next().and_then(|s| s.parse().ok()).unwrap_or(0);
+  let h: usize = hi.next().and_then(|s| s.parse().ok()).unwrap_or(0);
+  let _ox: i32 = hi.next().and_then(|s| s.parse().ok()).unwrap_or(0);
+  let _oy: i32 = hi.next().and_then(|s| s.parse().ok()).unwrap_or(0);
+  let multi_level = hi.next() == Some("ZRANGE");
   let mut tiles = vec![vec![Tile::Grass; w]; h];
   let mut objects = Vec::new();
-  for y in 0..h {
+  let read_row = |line: &str, y: usize, tiles: &mut Vec<Vec<Tile>>, objects: &mut Vec<(i32, i32, Object)>| {
+    let mut toks = line.split_whitespace();
     for x in 0..w {
       let tile_tok = toks.next().unwrap_or("Grass");
       let obj_tok = toks.next().unwrap_or("-");
@@ -68,13 +71,39 @@ fn parse_save(data: &str) -> ParsedLevel {
         objects.push((x as i32, y as i32, obj));
       }
     }
+  };
+  if multi_level {
+    let mut cur_z: Option<i32> = None;
+    let mut cur_y = 0usize;
+    for line in lines {
+      if let Some(rest) = line.strip_prefix("LEVEL ") {
+        cur_z = rest.trim().parse().ok();
+        cur_y = 0;
+      } else if cur_z == Some(target_z) && cur_y < h {
+        read_row(line, cur_y, &mut tiles, &mut objects);
+        cur_y += 1;
+      }
+    }
+  } else if target_z == 0 {
+    let body: String = lines.collect::<Vec<_>>().join("\n");
+    let mut toks = body.split_whitespace();
+    for y in 0..h {
+      for x in 0..w {
+        let tile_tok = toks.next().unwrap_or("Grass");
+        let obj_tok = toks.next().unwrap_or("-");
+        tiles[y][x] = Tile::from_save(tile_tok).unwrap_or(Tile::Grass);
+        if obj_tok != "-" && let Some(obj) = object_from_save(obj_tok) {
+          objects.push((x as i32, y as i32, obj));
+        }
+      }
+    }
   }
   ParsedLevel { width: w, height: h, tiles, objects }
 }
 
 pub fn generate() -> Location {
-  let surface = parse_save(SURFACE_DATA);
-  let underground = parse_save(UNDERGROUND_DATA);
+  let surface = parse_save(ISLAND_DATA, 0);
+  let underground = parse_save(ISLAND_DATA, -1);
   let w = surface.width.max(underground.width);
   let h = surface.height.max(underground.height);
 
